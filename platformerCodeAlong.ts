c4005707f17b4be1759ace13e9f9122cd4f47c8c
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2025-09-17 16:47:34 -0700

    Refactor, add goal validation

--- platformerCodeAlong.ts
+++ platformerCodeAlong.ts
@@ -17,64 +17,87 @@ namespace platformer_code_along {
         // Run the user's code
         handler();
 
-        if (stepNumber === 1) {
-            validateTilemapWithAtLeastTwoTiles(); // Create tilemap, sky & ground
-        } else if (stepNumber === 2) {
-            validateTilemapWithAtLeastThreeTiles(); // Add hazard tile
-        } else if (stepNumber === 3) {
-            validateWallsInTilemap(); // Add walls
+        let valid = true;
+        switch (stepNumber) {
+            case 1:
+                // Create tilemap, sky & ground
+                valid = validateTilemapExists();
+                if (!valid) break;
+                valid = validateTilemapTileCount(2, "It looks like you only have one tile in your tilemap. Add a second one to continue!");
+                if (!valid) break;
+                valid = validateTilemapResized();
+
+                break;
+            case 2:
+                // Add hazard tile
+                valid = validateTilemapTileCount(3, "Add a new hazard tile to continue!");
+                break;
+            case 3:
+                // Add walls
+                valid = validateWallsInTilemap();
+                break;
+            case 4:
+                // Add goal
+                valid = validateTilemapTileCount(4, "Add a new goal tile to continue!");
+            default:
+                // No validation
+        }
+
+        if (valid) {
+            tutorialcontrols.sendValidationResult(true, "Nice work!");
         }
     }
 
     /*********************************
     * VALIDATION
     *********************************/
-    function validateTilemapWithAtLeastTwoTiles() {
+    function validateTilemapExists(): boolean {
         const tilemap = game.currentScene().tileMap;
 
         if (!tilemap) {
             tutorialcontrols.sendValidationResult(false, "Create a tilemap with at least two tiles to continue!");
-            return;
+            return false;
         }
 
-        const tileThreshold = 2;
+        return true;
+    }
+
+    function validateTilemapTileCount(tileThreshold: number, errMsg: string): boolean {
+        const tilemap = game.currentScene().tileMap;
         const uniqueTiles = getUniqueTileImageCount(tilemap);
-        if (uniqueTiles < tileThreshold) {
-            tutorialcontrols.sendValidationResult(false, "It looks like you only have one tile in your tilemap. Add a second one to continue!");
-            return;
-        }
 
+        if (uniqueTiles < tileThreshold) {
+            tutorialcontrols.sendValidationResult(false, errMsg);
+            return false;
+        }
+        return true;
+    }
+
+    function validateTilemapResized(): boolean {
+        const tilemap = game.currentScene().tileMap;
         const defaultWidth = 16;
         const defaultHeight = 16;
         if (tilemap.data.width == defaultWidth && tilemap.data.height == defaultHeight) {
             tutorialcontrols.sendValidationResult(true, "Great! Double check before proceeding: do you want to resize your tilemap?");
-            return;
+            return false;
         }
 
-        tutorialcontrols.sendValidationResult(true, "Great job!");
+        return true;
     }
 
-    function validateTilemapWithAtLeastThreeTiles() {
-        const tilemap = game.currentScene().tileMap;
-        const tileThreshold = 3;
-        const uniqueTiles = getUniqueTileImageCount(tilemap);
-        if (uniqueTiles < tileThreshold) {
-            tutorialcontrols.sendValidationResult(false, "Add a new hazard tile to continue!");
-            return;
-        }
-    }
-
-    function validateWallsInTilemap() {
+    function validateWallsInTilemap(): boolean {
         const tilemap = game.currentScene().tileMap;
 
         for (let c = 0; c < tilemap.data.width; c++) {
             for (let r = 0; r < tilemap.data.height; r++) {
                 if (tilemap.data.isWall(c, r)) {
                     tutorialcontrols.sendValidationResult(true, "Nice work!");
+                    return true;
                 }
             }
         }
         tutorialcontrols.sendValidationResult(false, "Add walls in your tilemap to continue!");
+        return false;
     }
 
     /*********************************
